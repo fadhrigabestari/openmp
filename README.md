13a1c11056ac6ac9a08f969476bbd35bfcdf6124
yulyulyharuka <yuly_haruka@yuly.haruka-VirtualBox>
2019-03-01 22:38:17 +0700

    final program

--- README.md
+++ README.md
@@ -1,3 +1,92 @@
 # Petunjuk Penggunaan Program
+<br>
+1. Open your terminal from directory that possess this file
+2. Type 'make' to compile this program
+3. Type './radix_sort threads_count' to run the program
+<br>
 # Pembagian tugas
+- Yuly Haruka Berliana Gunawan / 13516031
+- M. Fadhriga Bestari / 13516154
+<br>
 # Laporan Pengerjaan
+## Deskripsi Solusi Paralel
+Pada program yang telah kami buat, pada algortima radix sort terdapat banyak looping untuk mencari flag, index down, index up, dan permutasi dalam proses radix sort. Setiap looping yang terjadi pada radix sort bersifat independen ( data pada looping tidak bergantung pada looping lainnya ) kecuali looping pada proses pengisian index down dan index up. Maka dari itu, kami mengimplementasikan OpenMP paralel pada looping yang terdapat pada algoritma radix sort sehingga per-looping nya akan dibagi menjadi banyak nya thread yang diinginkan. 
+<br>
+
+## Analisis Solusi
+Menurut kami, penerapan OpenMP paralel pada radix sort dapat mempercepat proses sorting karena dengan dilakukannya pembagian proses pada setiap thread, proses pengisian array flag dan proses permutasi yang dilakukan dalam radix sort dipercepat.
+<br>
+
+## Jumlah thread yang digunakan
+Jumlah thread yang digunakan adalah 4 karena sesuai dengan core yang dialokasikan pada Virtual Machine yang kami gunakan untuk menjalankan program. 
+<br>
+
+## Pengukuran Kinerja
+<br>
+- N = 5000 dengan thread = 4
+Uji Coba 1
+Radix Sort dengan paralel : 21672 ms
+Radix Sort dengan serial : 23079 ms
+
+Uji Coba 2
+Radix Sort dengan paralel : 21409 ms
+Radix Sort dengan serial : 22530 ms
+
+Uji Coba 3
+Radix Sort dengan paralel : 19414 ms
+Radix Sort dengan serial : 20562 ms
+
+- N = 50000 dengan thread = 4
+Uji Coba 1
+Radix Sort dengan paralel : 105996 ms
+Radix Sort dengan serial : 117882 ms
+
+Uji Coba 2
+Radix Sort dengan paralel : 106440 ms
+Radix Sort dengan serial : 118108 ms
+
+Uji Coba 3
+Radix Sort dengan paralel : 105162 ms
+Radix Sort dengan serial : 122656 ms
+
+- N = 100000 dengan thread = 4
+Uji Coba 1
+Radix Sort dengan paralel : 220076 ms
+Radix Sort dengan serial : 244303 ms
+
+Uji Coba 2
+Radix Sort dengan paralel : 195386 ms
+Radix Sort dengan serial : 218235 ms
+
+Uji Coba 3
+Radix Sort dengan paralel : 195626 ms
+Radix Sort dengan serial : 219280 ms
+
+- N = 200000 dengan thread = 4
+Uji Coba 1
+Radix Sort dengan paralel : 450837 ms
+Radix Sort dengan serial : 495986 ms
+
+Uji Coba 2
+Radix Sort dengan paralel : 387034 ms
+Radix Sort dengan serial : 433822 ms
+
+Uji Coba 3
+Radix Sort dengan paralel : 362393 ms
+Radix Sort dengan serial : 408307 ms
+
+- N = 400000 dengan thread = 4
+Uji Coba 1
+Radix Sort dengan paralel : 946567 ms
+Radix Sort dengan serial : 1.04382e+06 ms
+
+Uji Coba 2
+Radix Sort dengan paralel : 705935 ms
+Radix Sort dengan serial : 807857 ms
+
+Uji Coba 3
+Radix Sort dengan paralel : 712231 ms
+Radix Sort dengan serial : 801536 ms
+<br>
+## Analisis Perbandingan kinerja serial dan paralel
+Berdasarkan hasil uji coba yang telah dilakukan, untuk setiap N, kinerja dari radix sort paralel lebih baik dibandingkan dengan radix sort serial. Rata-rata perbedaan waktu antara keduanya adalah 0.1 s
--- myradix.cpp
+++ myradix.cpp
@@ -30,11 +30,63 @@ void rng(int* arr, int n) {
   }
 }
 
+// serial radix sort from geeks for geeks
+// A function to do counting sort of arr[] according to 
+// the digit represented by exp. 
+void countSort(int arr[], int n, int exp) 
+{ 
+    int output[n]; // output array 
+    int i, count[10] = {0}; 
+  
+    // Store count of occurrences in count[] 
+    for (i = 0; i < n; i++) 
+        count[ (arr[i]/exp)%10 ]++; 
+  
+    // Change count[i] so that count[i] now contains actual 
+    //  position of this digit in output[] 
+    for (i = 1; i < 10; i++) 
+        count[i] += count[i - 1]; 
+  
+    // Build the output array 
+    for (i = n - 1; i >= 0; i--) 
+    { 
+        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i]; 
+        count[ (arr[i]/exp)%10 ]--; 
+    } 
+  
+    // Copy the output array to arr[], so that arr[] now 
+    // contains sorted numbers according to current digit 
+    for (i = 0; i < n; i++) 
+        arr[i] = output[i]; 
+} 
+  
+// The main function to that sorts arr[] of size n using  
+// Radix Sort 
+void serialRadixSort(int arr[], int n) 
+{ 
+    // Find the maximum number to know number of digits 
+    int m = getMaxElement(arr, n); 
+  
+    // Do counting sort for every digit. Note that instead 
+    // of passing digit number, exp is passed. exp is 10^i 
+    // where i is current digit number 
+    clock_t beginTime = clock();
+    for (int exp = 1; m/exp > 0; exp *= 10){
+      countSort(arr, n, exp);
+    }
+    clock_t endTime = clock();
+
+    double elapsedTime = (double) endTime - beginTime / CLOCKS_PER_SEC;
+
+    cout << "Serial Radix Sort Time: " << elapsedTime << endl;
+}
+
+// parallel radix sort
 // get specific bit at index = idx
-int* generateFlag(int* arr, int n, int idx) {
+int* generateFlag(int* arr, int n, int idx, int thread_count) {
   int* flag = (int*) malloc(n * sizeof(int));
 
-  // parallel this
+  #pragma omp parallel for num_threads(thread_count)
   for (int i = 0; i < n; i++) {
     if ((arr[i] >> idx) & 1 == 1) {
       flag[i] = 1;
@@ -75,10 +127,10 @@ int* generateIUp(int* flag, int n) {
   return iUp;
 }
 
-int* generateShouldIndex(int* flag, int* iDown, int* iUp, int n) {
+int* generateShouldIndex(int* flag, int* iDown, int* iUp, int n, int thread_count) {
   int* shouldIndex = (int*) malloc(n * sizeof(int));
 
-  // parallel this
+  #pragma omp parallel for num_threads(thread_count)
   for (int i = 0; i < n; i++) {
     if (flag[i] == 0) {
       shouldIndex[i] = iDown[i];
@@ -90,49 +142,68 @@ int* generateShouldIndex(int* flag, int* iDown, int* iUp, int n) {
 }
 
 // permute
-void permute(int* arr, int* flag, int* iDown, int* iUp, int n) {
+void permute(int* arr, int* flag, int* iDown, int* iUp, int n, int thread_count) {
   int* shouldArr = (int*) malloc(n * sizeof(int));
 
-  int* shouldIndex = generateShouldIndex(flag, iDown, iUp, n);
+  int* shouldIndex = generateShouldIndex(flag, iDown, iUp, n, thread_count);
 
-  // parallel this
+  #pragma omp parallel for num_threads(thread_count)
   for (int i = 0; i < n; i++) {
     shouldArr[shouldIndex[i]] = arr[i];
   }
 
-  // parallel this
+  #pragma omp parallel for num_threads(thread_count)
   for (int i = 0; i < n; i++) {
     arr[i] = shouldArr[i];
   }
 }
 
-void split(int* arr, int n, int idx) {
-  int* flag = generateFlag(arr, n, idx);
+void split(int* arr, int n, int idx, int thread_count) {
+  int* flag = generateFlag(arr, n, idx, thread_count);
   int* iDown = generateIDown(flag, n);
   int* iUp = generateIUp(flag, n);
 
-  permute(arr, flag, iDown, iUp, n);
+  permute(arr, flag, iDown, iUp, n, thread_count);
 }
 
-void radixSort(int* arr, int n) {
+void radixSort(int* arr, int n, int thread_count) {
   int maxElement = getMaxElement(arr, n);
   int idx = 0;
 
   while ((maxElement >> idx) > 0) {
-    split(arr, n, idx);
+    split(arr, n, idx, thread_count);
     idx++;
   }
 }
 
-int main(int argc, char** agrv) {
-  int n = 100000;
+int main(int argc, char** argv) {
+  int thread_count;
+
+  if (argc != 2){
+    cout << "Wrong input" << endl;
+    cout << "./radix_sort thread_count" << endl;
+    exit(0);
+  } else {
+    thread_count = stoi(argv[1]);
+  }
+  
+  int n;
+  cout << "Insert the size of the array :" << endl;
+  cin >> n;
   int * arr = (int*) malloc(n * sizeof(int));
+  int * c_arr = (int*) malloc(n * sizeof(int));
+
   rng(arr, n);
+  for (int i = 0; i< n; i++){
+    c_arr[i] = arr[i];
+  }
+
   clock_t beginTime = clock();
-  radixSort(arr, n);
+  radixSort(arr, n, thread_count);
   clock_t endTime = clock();
 
   double elapsedTime = (double) endTime - beginTime / CLOCKS_PER_SEC;
 
-  cout << "Time: " << elapsedTime;
+  cout << "Parallel Radix Sort Time: " << elapsedTime << endl;
+  serialRadixSort(c_arr, n);
 }
